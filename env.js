b77f6fa9050de647516586b21946118bf51c9efe
permissionlessteamraj11 <jiovinitkumarjio@gmail.com>
2026-03-18 12:43:19 +0530

    Create env.js

---
+++ env.js
@@ -0,0 +1,6 @@
+// env.js  ← ye file .gitignore mein daalo, alag se deploy karo
+window.__ENV = {
+  SUPABASE_URL:      'https://jrjzkpxlpiovhwithxbo.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyanprcHhscGlvdmh3aXRoeGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyNDUzNDQsImV4cCI6MjA4NDgyMTM0NH0.aRbGjlOVUrRK5Ji-P1W-LJ6HUVYuJeU2pixABEWCYYY',
+  SITE_URL:          'https://www.elitehosting.in',
+};
